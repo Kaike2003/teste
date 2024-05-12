6a2e4f94bf54aca6ee00926ce8f6754a104748b3
Kaike Bartolomeu <102388705+Kaike2003@users.noreply.github.com>
2024-05-12 19:56:24 +0100

    teste arduino

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Teste from "./components/Teste"
+
+export const App: React.FC<{}> = () => {
+
+    return (
+        <div>
+            <Teste />
+        </div>
+    )
+
+}
